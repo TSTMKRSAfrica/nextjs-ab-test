f5571305a526ef734b15eb42f683abbf3aa53c1b
joselevelsup <hipnotickid12@gmail.com>
2020-11-04 12:33:01 -0500

    working on more tests; working on build

--- tests/experiment.test.js
+++ tests/experiment.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, render} from 'enzyme';
 import {Experiment, Variant} from '../src';
 
 describe('Experiment - shallow', () => {
@@ -34,11 +34,72 @@ describe('Experiment - shallow', () => {
 		expect(component.contains(<div>Variant A</div>)).toBe(true);
 	});
 
-	it("should throw an error if Variants aren't included", () => {
-		const component = shallow(<Experiment name="Experiment-test"></Experiment>);
+	it("should render Variant B with a weight of 70", () => {
+		const component = shallow(
+			<Experiment weights={[30, 70]} name="Experiment-test">
+				<Variant name="A">
+					<div>Variant A</div>
+				</Variant>
+				<Variant name="B">
+					<div>Variant B</div>
+				</Variant>
+			</Experiment>
+		);
 
-		expect(component).toThrow();
+		expect(component.contains(<div>Variant B</div>)).toBe(true);
+	});
+
+	it("should fire the experiment load function", () => {
+		const experimentDidLoad = (experiment, variant) => {
+			expect(typeof experiment).toBe("string");
+			expect(experiment).toBe("Experiment-test");
+			expect(typeof variant).toBe("string");
+			expect(variant == "A" || variant == "B").toBeTruthy();
+		};
+
+		shallow(
+			<Experiment onExperimentLoad={experimentDidLoad} name="Experiment-test">
+				<Variant name="A">
+					<div>Variant A</div>
+				</Variant>
+				<Variant name="B">
+					<div>Variant B</div>
+				</Variant>
+			</Experiment>
+		);
+	})
+});
+
+describe.only('Experiment - render (as HTML)', () => {
+	it('should render the explicitly set variant', () => {
+		const component = render(
+			<Experiment activeVariant="A" name="Experiment-test">
+				<Variant name="A">
+					<div>Variant A</div>
+				</Variant>
+				<Variant name="B">
+					<div>Variant B</div>
+				</Variant>
+			</Experiment>
+		);
+
+		expect(component.html()).toContain("Variant A");
+	});
+
+	it.only("should render Variant B with a weight of 70", () => {
+		const component = render(
+			<Experiment weights={[30, 70]} name="Experiment-test">
+				<Variant name="A">
+					<div>Variant A</div>
+				</Variant>
+				<Variant name="B">
+					<div>Variant B</div>
+				</Variant>
+			</Experiment>
+		);
+
+		expect(component.html()).toContain("Variant B");
 	});
 });
 
-describe('Experiment - render (as HTML)', () => {});
+
